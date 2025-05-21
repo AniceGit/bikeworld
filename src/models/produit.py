80572b984fcdb2785d27935ef343da2782b9db5b
stephane <Vous@exemple.com>
2025-05-21 09:45:45 +0200

    update

--- src/models/produit.py
+++ src/models/produit.py
@@ -2,6 +2,7 @@ class produit():
     def __init__(self, id:int, nom:str, description:str, spec_tech:str, couleur:str, image:str, prix:float, stock:int) -> None:
         '''
         Initialisation de la classe produit
+        
         '''
         self.id = id
         self.nom = nom
