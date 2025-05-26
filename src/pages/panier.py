801ac276fe958862a9f909d05257db1bcd99e888
Sebastien Dewaelle <sdewaelle@gmail.com>
2025-05-26 13:04:04 +0200

    Modification des frais de livraison

--- src/pages/panier.py
+++ src/pages/panier.py
@@ -93,7 +93,7 @@ else:
             st.session_state.panier = {
                 "date_panier": str(datetime.date.today()),
                 "total_panier": 0.0,
-                "frais_livraison": 20.0,
+                "frais_livraison": 25.0,
                 "liste_produits_quantite": [],
             }
 
